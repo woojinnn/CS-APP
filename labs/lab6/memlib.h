1bdb2e0f1433e4bd43d8f5d6fc05bbf8cbd49a22
WoojinLee <wjl0209@kaist.ac.kr>
2021-06-02 07:28:48 +0000

    book version

--- labs/lab6/memlib.h
+++ labs/lab6/memlib.h
@@ -2,9 +2,25 @@
 
 void mem_init(void);
 void mem_deinit(void);
+
+/*
+ * Expands the heap by incr bytes, where incr is a positive non-zero integer and
+ * returns a generic pointer to the first byte of the newly allocated heap area.
+ * The semantics are identical to the Unix sbrk function, except that mem_sbrk
+ * accepts only a positive non-zero integer argument.
+ */
 void *mem_sbrk(int incr);
+
 void mem_reset_brk(void);
+
+/* Returns a generic pointer to the first byte in the heap. */
 void *mem_heap_lo(void);
+
+/* Returns a generic pointer to the last byte in the heap. */
 void *mem_heap_hi(void);
+
+/* Returns the current size of the heap in bytes. */
 size_t mem_heapsize(void);
+
+/* Returns the system’s page size in bytes (4K on Linux systems). */
 size_t mem_pagesize(void);
